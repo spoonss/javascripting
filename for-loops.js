adc9c1f59ac623f53b3c840bd0e60bf9c2af6ed1
Charles Wong <nok.wong@gmail.com>
2018-08-04 19:21:40 +0000

    Initial commit with all my work so far

---
+++ for-loops.js
@@ -0,0 +1,11 @@
+/*var limit = 10;
+for (var total = 0; total < limit; total++ ){
+console.log(total);
+}*/
+
+var total = 0;
+var limit = 10;
+for (var i = 0; i < limit; i++) {
+  total += i;
+}
+console.log(total);
